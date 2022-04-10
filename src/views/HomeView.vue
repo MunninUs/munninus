6454c7e392f8a51512f6ebc87ddd564f481c0f27
João Conrad <65290519+Conrad-C@users.noreply.github.com>
2022-04-10 18:34:32 -0300

    Change slide on button click

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,29 +5,28 @@
       <img :src="require('../assets/logo-'+theme+'.svg')" />
     </header>
     <main class="align-center flex-column">
-      <div class="flex-column space-between align-center gap-2">
-        <h1>Plan your day</h1>
-        <img :src="require('../assets/plan-'+theme+'.svg')" />
-        <p>
-          Plan your day and save your incredible ideas in the fastest away and
-          format possible.
-        </p>
-      </div>
+      <WelcomeSlide v-show="currentSlide === 0" :slide="slides[0]" :theme="theme" />
+      <WelcomeSlide v-show="currentSlide === 1" :slide="slides[1]" :theme="theme" />
       <div class="flex gap-1 margin-top-2">
-        <div class="circle selected"></div>
-        <div class="circle"></div>
+        <div :class="`circle ${currentSlide === 0 ? 'selected' : ''}`"></div>
+        <div :class="`circle ${currentSlide === 1 ? 'selected' : ''}`"></div>
       </div>
     </main>
-    <footer>
-      <button>Next</button>
+    <footer v-show="currentSlide === 0">
+      <button @click="currentSlide = 1">Next</button>
+    </footer>
+    <footer v-show="currentSlide === 1">
+      <button>Get Started</button>
     </footer>
   </div>
 </template>
 
 <script>
+import WelcomeSlide from '@/components/WelcomeSlide.vue';
 // @ is an alias to /src
 
 export default {
+  components: { WelcomeSlide },
   name: 'HomeView', 
   data(){
     return {
@@ -38,7 +37,13 @@ export default {
           image: 'plan',
           text: 'Plan your day and save your incredible ideas in the fastest away and format possible.'
         },
+        {
+          title: 'Organize your tasks',
+          image: 'organize',
+          text: 'Here you can organize your tasks easily, customizing, and sorting the way the fits you better.'
+        },
       ],
+      currentSlide:0,
       theme:document.body.getAttribute('theme') ? document.body.getAttribute('theme') : 'light',
     }
   },
