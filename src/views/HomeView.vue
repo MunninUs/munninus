a09a8f34e992029f9a3aa6bc0db0b5128fa9c239
João Conrad <65290519+Conrad-C@users.noreply.github.com>
2022-04-09 21:45:05 -0300

    Welcome screen without slide and themes settings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="welcome">
+    <button @click="changeTheme()">Change theme</button>
+    <header>
+      <img :src="require('../assets/logo-'+theme+'.svg')" />
+    </header>
+    <main class="align-center flex-column">
+      <div class="flex-column space-between align-center gap-2">
+        <h1>Plan your day</h1>
+        <img :src="require('../assets/plan-'+theme+'.svg')" />
+        <p>
+          Plan your day and save your incredible ideas in the fastest away and
+          format possible.
+        </p>
+      </div>
+      <div class="flex gap-1 margin-top-2">
+        <div class="circle selected"></div>
+        <div class="circle"></div>
+      </div>
+    </main>
+    <footer>
+      <button>Next</button>
+    </footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView', 
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      darkMode:false,
+      slides: [
+        {
+          title: 'Plan your day',
+          image: 'plan',
+          text: 'Plan your day and save your incredible ideas in the fastest away and format possible.'
+        },
+      ],
+      theme:document.body.getAttribute('theme') ? document.body.getAttribute('theme') : 'light',
+    }
+  },
+  methods: {
+    changeTheme() {
+      this.darkMode = !this.darkMode;
+    },
+  },
+  watch:{
+    darkMode(newData) {
+      if (newData) {
+        document.body.setAttribute("theme", "dark");
+        this.theme = 'dark';
+      } else {
+        document.body.setAttribute("theme", "light");
+        this.theme = 'light';
+      } 
+    }
+  },
 }
 </script>
+<style>
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    <h3>Pão com batata</h3>
+  <div class="login">
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      darkMode: false, 
+    };
+  },
+  methods: {
+    changeTheme() {
+      if (this.darkMode) {
+        document.body.setAttribute("theme", "light");
+        this.darkMode = false;
+      } else {
+        document.body.setAttribute("theme", "dark");
+        this.darkMode = true;
+      }
+    },
+  },
+};
 </script>
 
 <style>
