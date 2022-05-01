c48d512ccc28ae2439812ddc22a71c766dfed4e1
João Conrad <65290519+Conrad-C@users.noreply.github.com>
2022-05-01 12:04:43 -0300

    Toggle visibility button in input password

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="login">
     <header class="flex space-between align-center">
-      <img :src="require('../assets/logo-'+theme+'.svg')" />
+      <img :src="require('../assets/logo-' + theme + '.svg')" />
       <label class="switch">
-        <input v-model="darkMode" type="checkbox">
+        <input v-model="darkMode" type="checkbox" />
         <span class="slider" :theme="theme"></span>
       </label>
     </header>
@@ -12,18 +12,20 @@
       <h2>Login to your account</h2>
       <p>We are happy to have you back!</p>
       <div class="flex-column gap-2">
-        <input placeholder="example@gmail.com"> 
-        <input placeholder="Password" type="password">
+        <input placeholder="example@gmail.com" />
+        <div class="input-box">
+          <input ref="password-input" placeholder="Password" type="password" />
+          <span @click="showPassword()" class="material-symbols-outlined">
+            {{ viewPasswordIcon }}
+          </span>
+        </div>
         <a>Forgot password?</a>
       </div>
     </main>
     <footer>
       <button>Login</button>
-      <div>
-        Or
-      </div>
-      <div id="google-login">
-      </div>
+      <div>Or</div>
+      <div id="google-login"></div>
     </footer>
   </div>
 </template>
@@ -32,30 +34,41 @@
 export default {
   data() {
     return {
-      darkMode: false, 
+      darkMode: false,
+      viewPasswordIcon: 'visibility'
     };
   },
   methods: {
+    showPassword(){
+      let input = this.$refs['password-input'];
+      if(this.viewPasswordIcon == 'visibility'){
+        input.setAttribute('type', 'text');
+        this.viewPasswordIcon = 'visibility_off'
+      } else {
+        input.setAttribute('type', 'password');
+        this.viewPasswordIcon = 'visibility';
+      }
+    },
     changeTheme() {
       this.darkMode = !this.darkMode;
     },
   },
-  watch:{
+  watch: {
     darkMode(newData) {
       if (newData) {
         document.body.setAttribute("theme", "dark");
-        this.$store.commit('SET_THEME', 'dark');
+        this.$store.commit("SET_THEME", "dark");
       } else {
         document.body.setAttribute("theme", "light");
-        this.$store.commit('SET_THEME', 'light');
-      } 
-    }
+        this.$store.commit("SET_THEME", "light");
+      }
+    },
   },
-  computed:{
-    theme(){
-      return this.$store.getters['GET_THEME'];
-    }
-  }
+  computed: {
+    theme() {
+      return this.$store.getters["GET_THEME"];
+    },
+  },
 };
 </script>
 
